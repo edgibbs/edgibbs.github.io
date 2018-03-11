048d68157fc904201ceddf756a1984ca97c25674
Ed Gibbs <edward_gibbs@yahoo.com>
2018-03-10 21:50:24 -0800

    Trying again

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -26,4 +26,4 @@ git config user.name "Ed Gibbs"
 git add --all
 git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
 git remote -v
-git push --force origin +master
+git push --force origin +origin/master
